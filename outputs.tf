486f4c112b0c4d91a8b28208d1a7d5085e1a20d9
PavelSinelnik <pavel.sinelnik@regula.by>
2023-04-13 17:13:04 +0300

    add outputs for kubeconfig

--- outputs.tf
+++ outputs.tf
@@ -2,3 +2,8 @@ output "cluster_name" {
   description = "Cluster name"
   value       = module.gke_cluster.cluster_name
 }
+
+output "config" {
+  description = "Cluster name"
+  value       = module.gke_auth.kubeconfig_raw
+}
